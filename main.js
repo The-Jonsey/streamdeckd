2a16e86c0d30f8665d89cb03ee8b1c3f78894c7a
Louis Jones <louis@thejonsey.com>
2020-03-07 23:27:57 +0000

    Removed canvas depedency

--- main.js
+++ main.js
@@ -5,7 +5,7 @@ const fs = require('fs');
 const StreamDeck = require('elgato-stream-deck');
 const cp = require('child_process');
 const homeDir = require('os').homedir();
-const {createCanvas} = require('canvas');
+const pixelWidth = require('string-pixel-width');
 const usbDetect = require("usb-detection");
 let handlers = require("./handlers.json");
 let dbus = require("./dbus.js");
@@ -57,7 +57,7 @@ let interval;
 
 usbDetect.startMonitoring();
 
-process.stdin.resume();
+//process.stdin.resume();
 
 usbDetect.on("add:4057", async () => {
     await attemptConnection();
@@ -282,7 +282,11 @@ async function generateBuffer(icon = __dirname + "/blank.png", text, index) {
     buf.resize(72, 72);
     if (text) {
         let textBuf = await svgtoimg(textSVG);
-        textBuf = await jimp.read(textBuf);
+        try {
+            textBuf = await jimp.read(textBuf);
+        } catch (e) {
+            console.log(e);
+        }
         textBuf.resize(72, 72).quality(100)
             .flip(false, true)
             .flip(true, false);
@@ -313,11 +317,7 @@ function svgtoimg(svgString) {
 }
 
 function calculateFontSize(text) {
-    let fontFamily = "16px sans-serif";
-    const canvas = createCanvas(72, 72);
-    const context = canvas.getContext('2d');
-    context.font = fontFamily;
-    let width = context.measureText(text).width;
+    let width = pixelWidth(text, { size: 16 });
     let size = (1 / (width / 72)) * 100;
     return size < 500 ? size > 50 ? size : 50 : 500;
 }
